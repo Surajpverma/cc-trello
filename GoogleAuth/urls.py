9835d341941fed13ea753eced96ae92fc7441cdc
Sanyam-Garg <76063886+Sanyam-Garg@users.noreply.github.com>
2022-09-12 23:48:07 +0530

    Revert "initial"

--- GoogleAuth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-from .views import GoogleLogin 
-
-urlpatterns = [
-    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
-]
